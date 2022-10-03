9662f012ccc69e4db3da013f14c4e1e1dd965de4
Sergei Minakov <sergei.minakov@kcl.ac.uk>
2022-10-03 22:03:42 +0300

    fix Thing model

--- things/models.py
+++ things/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Thing():
+class Thing(models.Model):
     name = models.CharField(models.Model)
     description = models.TextField()
     quantity = models.IntegerField()
